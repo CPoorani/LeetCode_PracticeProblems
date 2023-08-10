62d86561de6b4caa23300247bab729fd94549c9b
CPoorani <pooranimenon5@gmail.com>
2023-08-10 18:06:52 +0530

    1232_CheckStraightLine

---
+++ PracticeProblems/src/main/java/mandatoryHomeWork/foundation/P50_CheckStraightLine.java
@@ -0,0 +1,54 @@
+package mandatoryHomeWork.foundation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P50_CheckStraightLine {
+	/*
+	 * (1,2) (2,3) (3,4) (4,5) (5,6) (6,7)
+	 * Slope = (y2-y1) / (x2-x1) -- Between P1 and P2
+	 * Slope = (y3-y1) / (x3-x1) -- Between P1 and P3
+	 * (y2-y1)*(x3-x1) == (x2-x1)*(y3-y1)
+	 * 
+	 * Find the difference between P1 and P2 
+	 * 	For X, X2-X1
+	 * 	For Y, Y2-Y1
+	 * Iterate through the input elements
+	 * 		find the coordinates for the given i
+	 * 		Multiply ==> (y2-y1)*(x3-x1) != (x2-x1)*(y3-y1)
+	 * 			return false
+	 * else
+	 * 			return true
+	 * 
+	 */
+
+	public class CheckStraightLine {
+
+		@Test
+		public void test() {
+			int[][] actual = new int[][] {{ 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 6, 7 }};
+			Assert.assertEquals(true, checkLine(actual));
+		}
+
+		@Test
+		public void test1() {
+			int[][] actual = new int[][] {{ 1, 1 }, { 2, 2 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 67, 7 }};
+			Assert.assertEquals(false, checkLine(actual));
+		}
+
+		public boolean checkLine(int[][] coordinates) {
+			int differenceX = coordinates[1][0] - coordinates[0][0];
+			int differenceY = coordinates[1][1] - coordinates[0][1];
+			for (int i = 2; i < coordinates.length; i++) {
+				if (differenceY * (coordinates[i][0] - coordinates[0][0]) != differenceX
+						* (coordinates[i][1] - coordinates[0][1]))
+					return false;
+			}
+
+			return true;
+
+		}
+	}
+
+
+}
